7a5e8b6db2c9ee27fa19050418c0eff37a36c106
RhogHub <rhog.dev@gmail.com>
2023-12-13 11:05:36 -0300

    with API e2e test

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -22,16 +22,7 @@ describe("E2E test for product", () => {
         expect(response.status).toBe(200);
         expect(response.body.name).toBe("Teclado");
         expect(response.body.price).toBe(133.33);        
-    });
-
-    it("should not create a product",async () => {
-        const response = await request(app)
-            .post("/product")
-            .send({
-                name: "",               
-            });
-        expect(response.status).toBe(500);
-    });
+    });    
 
     it("should not create a product",async () => {
         const response = await request(app)
