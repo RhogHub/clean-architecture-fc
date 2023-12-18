3be8e38e371f3cafb1051af67cc4365f5b2a0662
RhogHub <rhog.dev@gmail.com>
2023-12-18 11:06:11 -0300

    API with presenters and lib to convert json to XML

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -77,6 +77,26 @@ describe("E2E test for customer", () => {
         const customer2 = listResponse.body.customers[1];
         expect(customer2.name).toBe("Jane");
         expect(customer2.address.street).toBe("Street 2");
+
+        //para testar presenter XML
+        const listResponseXML = await request(app)
+        .get("/customer")
+        .set("Accept", "application/xml")
+        .send();
+
+        expect(listResponseXML.status).toBe(200);
+        expect(listResponseXML.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+        expect(listResponseXML.text).toContain(`<customers>`);
+        expect(listResponseXML.text).toContain(`<customer>`);
+        expect(listResponseXML.text).toContain(`<name>John</name>`);
+        expect(listResponseXML.text).toContain(`<address>`);
+        expect(listResponseXML.text).toContain(`<street>Street 1</street>`);
+        expect(listResponseXML.text).toContain(`<number>123</number>`);
+        expect(listResponseXML.text).toContain(`<city>City 1</city>`);
+        expect(listResponseXML.text).toContain(`<zip>12345</zip>`);
+        expect(listResponseXML.text).toContain(`<customer>`);
+        expect(listResponseXML.text).toContain(`<name>Jane</name>`);
+        expect(listResponseXML.text).toContain(`<street>Street 2</street>`);        
     });
 
 });
